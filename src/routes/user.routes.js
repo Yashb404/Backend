2e0c05df168590ca2fde155b8d1dcac83f536bea
Yash <yashbhardwaj7890@gmail.com>
2025-05-21 16:42:05 +0530

    Refactor user routes to include additional user-related endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentUserPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
@@ -23,6 +23,18 @@ router.route("/register").post(
 
     router.route("/logout").post(verifyJWT, logoutUser)
 
-
+    router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+    router.route("/current-user").get(verifyJWT,getCurrentUser)
+    router.route("/update-account").patch(verifyJWT,updateAccountDetails)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT,changeCurrentUserPassword)
+    
+    router.route("/cover-image").patch(verifyJWT,upload.single(
+        "/coverImage"
+    ),
+    updateUserCoverImage)
+    router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+    router.route("/history").get(verifyJWT,getWatchHistory)
+
+
 export default router
